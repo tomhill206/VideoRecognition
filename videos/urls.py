03feef1680f116086abc3774fac5082c01bfa3ee
tom_hill <tom.hill206@gmail.com>
2023-10-12 17:24:45 +0100

    implemented search function

--- videos/urls.py
+++ videos/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.video_list, name='video_list'),
+    path('search/', views.search, name='search'),
 ]
--- videos/views.py
+++ videos/views.py
@@ -1,8 +1,18 @@
 import boto3
+import django
 from django.shortcuts import render
 from .models import Video
 from django.contrib.auth.decorators import login_required
 from django.conf import settings
+
+import torch
+from sklearn.metrics.pairwise import cosine_similarity
+import os
+import ast
+from transformers import BertTokenizer, BertModel
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FamilyVideos.settings')
+django.setup()
 
 @login_required
 def video_list(request):
@@ -25,3 +35,51 @@ def generate_signed_url(bucket_name, object_key):
                                                           'Key': object_key},
                                                   ExpiresIn=settings.SIGNED_URL_EXPIRATION_TIME)
     return signed_url
+
+def search(request):
+    query = request.GET.get('query', '')  # Get the query from the URL parameter
+    
+    #breakpoint()
+    videos = generate_results(query)
+
+    for video in videos:
+        video.signed_url = generate_signed_url('video-archive-video-files', video.video_file)
+        video.thumbnail_signed_url = generate_signed_url('video-archive-thumbnail-files', video.thumbnail_file)
+
+    return render(request, 'search_results.html', {'videos': videos, 'query': query})
+
+def generate_results(query):
+    videos = Video.objects.all()
+
+    # Extract video embeddings and convert to PyTorch tensors
+    video_embeddings = [ast.literal_eval(video.embeddings)[0] for video in videos]
+
+    query_embedding = get_embedding(query)
+
+    # Calculate the cosine similarity between video embeddings and other_tensor
+    # The result will be a list of cosine similarity scores
+    cosine_scores = cosine_similarity(video_embeddings, [query_embedding])
+
+    sorted_videos = [video for _, video in sorted(zip(cosine_scores, videos), reverse=True)]
+
+    # Get the top 10 video IDs with the highest similarity
+    top_similarity_videos = sorted_videos[:20]
+
+    return top_similarity_videos
+    
+def get_embedding(content):
+
+    model_name = "bert-base-uncased"
+    tokenizer = BertTokenizer.from_pretrained(model_name)
+    model = BertModel.from_pretrained(model_name)
+    # Tokenize and encode the labels using BERT
+    inputs = tokenizer(content, return_tensors="pt", padding=True, truncation=True, max_length=40)
+    
+    # Get BERT embeddings for the labels
+    with torch.no_grad():
+        outputs = model(**inputs)
+    
+    # Extract the embeddings for the [CLS] token (the first token)
+    embeddings = outputs.last_hidden_state.mean(dim=1)
+    
+    return embeddings.tolist()[0]
